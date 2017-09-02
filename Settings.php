77ba95f8fe021fb1d01459fce9cd655f4b074b1a
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-02 20:16:20 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -1,5 +1,23 @@
 <?php
 namespace Dfe\Qiwi;
-// 2017-04-18
+// 2017-09-02 The «QIWI Wallet» PSP does not provide a test mode: https://mage2.pro/t/4443
 /** @method static Settings s() */
-final class Settings extends \Df\Payment\Settings {}
+final class Settings extends \Df\Payment\Settings {
+	/**
+	 * 2017-09-02 «API ID».
+	 * @return string
+	 */
+	function apiID() {return $this->v();}
+
+	/**
+	 * 2017-09-02 «API password».
+	 * @return string
+	 */
+	function password1() {return $this->p();}
+
+	/**
+	 * 2017-09-02 «Webhook password».
+	 * @return string
+	 */
+	function password2() {return $this->p();}
+}
