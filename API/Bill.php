2fb725d209daff049b617840c590217775b55f12
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-03 23:50:49 +0300

    no message

--- API/Bill.php
+++ API/Bill.php
@@ -7,6 +7,12 @@ use Df\Core\Exception as DFE;
 final class Bill extends \Df\API\Facade {
 	/**
 	 * 2017-09-03
+	 * «4.2. Creating an Invoice», page 7.
+	 * «4.2. Выставление счета пользователю», страница 7.
+	 * `[QIWI Wallet] The REST API specification (v.2.12)`, https://mage2.pro/t/3745
+	 * https://github.com/QIWI-API/pull-payments-docs/blob/40d48cf0/_pull-payments-api_en.html.md#request--put
+	 * https://github.com/QIWI-API/pull-payments-docs/blob/40d48cf0/_pull-payments-api_ru.html.md#Запрос--put
+	 * https://developer.qiwi.com/ru/pull-payments/index.html#invoice_rest
 	 * @param string $id
 	 * @param array(string => mixed) $a
 	 * @return O
