1329f68960c47a421c19b3eb913dcda622d6df4e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-06 02:37:25 +0300

    0.1.7

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,6 +1,8 @@
-// 2017-04-18
+// 2017-09-06
 define([
 	'Df_Payment/custom'
-], function(parent) {'use strict'; return parent.extend({
-	defaults: {df: {test: {showBackendTitle: false}}}
+], function(parent) {'use strict';
+/** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */
+return parent.extend({
+	defaults: {df: {formTemplate: 'Dfe_Qiwi/main'}}
 });});
