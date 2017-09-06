2d9636d409f910ee03107f99a2a756346f533356
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-06 05:43:51 +0300

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,6 +1,6 @@
 // 2017-09-06
 define([
-	'Df_Payment/custom'
+	'Df_Payment/custom', 'Df_Phone/lib/js/main'
 ], function(parent) {'use strict';
 /** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */
 return parent.extend({
