79f34a87befe9ddd7938ec6ee9aed82ff3d47175
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-07 07:56:25 +0300

    0.2.1

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,7 +1,7 @@
 // 2017-09-06
 define([
-	'Df_Payment/custom'
-], function(parent) {'use strict';
+	'df', 'Df_Payment/billingAddressChange', 'Df_Payment/custom', 'jquery'
+], function(df, baChange, parent, $) {'use strict';
 /** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */
 return parent.extend({
 	defaults: {df: {formTemplate: 'Dfe_Qiwi/main'}, phone: ''},
@@ -16,14 +16,36 @@ return parent.extend({
 	 * @see Df_Payment/mixin::dfData()
 	 * @returns {Object}
 	 */
-	dfData: function() {return df.o.merge(this._super(), {phone: this.phone()});},
+	dfData: function() {return df.o.merge(this._super(), {phone: df.s.normalizePhone(this.phone())});},
 	/**
 	 * 2017-09-06
 	 * @override
 	 * @see Df_Payment/custom::initialize()
 	 * @returns {exports}
 	*/
-	initialize: function() {this._super(); this.phone('+552131398000'); return this;},
+	initialize: function() {
+		this._super();
+		/** @type {Object} */ var c = window.checkoutConfig.customerData;
+		var _this = this;
+		baChange(function(a) {
+			var newValue = null;
+			if (a.telephone && a.telephone.length) {
+				newValue = a.telephone;
+			}
+			else if (c && c.telephone && c.telephone.length) {
+				newValue = c.telephone;
+			}
+			if (newValue) {
+				newValue = df.s.normalizePhone(newValue);
+				_this.phone(newValue);
+				var $form = _this.dfForm();
+				if ($form) {
+					$('[type=tel]', $form).intlTelInput('setNumber', newValue);
+				}
+			}
+		});
+		return this;
+	},
 	/**
 	 * 2017-09-06 The method should return `this` because it is used in a chain:
 	 *	this._super()
