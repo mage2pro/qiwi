b9115526646995cf354d95a0052ffc2846ab754e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-14 05:53:04 +0300

    no message

--- Signer.php
+++ Signer.php
@@ -20,6 +20,7 @@ abstract class Signer extends \Df\PaypalClone\Signer {
 	 * 2017-04-18
 	 * @override
 	 * @see \Df\PaypalClone\Signer::sign()
+	 * @used-by \Df\PaypalClone\Signer::_sign()
 	 * @return string
 	 */
 	final protected function sign() {return implode($this->values());}
