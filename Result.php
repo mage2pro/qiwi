84bded3051b972a0f8d3e7e7ee6d43e13669b322
Dmitry Fedyuk <dfediuk@gmail.com>
2021-12-03 07:39:13 +0600

    1.1.5

--- Result.php
+++ Result.php
@@ -1,40 +1,40 @@
 <?php
 namespace Dfe\Qiwi;
-use Magento\Framework\App\Response\Http as HttpResponse;
-use Magento\Framework\App\Response\HttpInterface as IHttpResponse;
 # 2017-09-12
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
-class Result extends \Df\Framework\W\Result {
+class Result extends \Df\Framework\W\Result\Xml {
 	/**
-	 * 2017-09-13
-	 * 2017-11-17
-	 * We can use the PHP «final» keyword here,
-	 * because the method is absent in @see \Magento\Framework\Controller\ResultInterface
+	 * 2021-12-03
 	 * @override
-	 * @see \Df\Framework\W\Result::__toString()
-	 * @used-by render()
-	 * @used-by \Df\Payment\W\Action::execute()
-	 * @return string
+	 * @see \Df\Framework\W\Result\Xml::contents()
+	 * @used-by \Df\Framework\W\Result\Xml::__toString()
+	 * @return array(string => mixed)
 	 */
-	final function __toString() {return df_xml_g('result', ['result_code' => $this->_code]);}
+	protected function contents() {return ['result_code' => $this->_code];}
 
 	/**
-	 * 2017-09-13
+	 * 2017-09-13, 2021-12-03
 	 * QIWI Wallet requires it to be «text/xml», not «application/xml»:
 	 * https://github.com/QIWI-API/pull-payments-docs/blob/40d48cf0/_notification_en.html.md#requirements-to-the-response-for-notification
 	 * @override
-	 * @see \Df\Framework\W\Result::render()
-	 * @used-by \Df\Framework\W\Result::renderResult()
-	 * @param IHttpResponse|HttpResponse $r
+	 * @see \Df\Framework\W\Result\Xml::contentType()
+	 * @used-by \Df\Framework\W\Result\Xml::render()
+	 * @return string
 	 */
-	final protected function render(IHttpResponse $r) {
-		$r->setBody($this->__toString());
-		df_response_content_type('text/xml', $r);
-	}
+	protected function contentType() {return 'text/xml';}
+
+	/**
+	 * 2021-12-03
+	 * @override
+	 * @see \Df\Framework\W\Result\Xml::tag()
+	 * @used-by \Df\Framework\W\Result\Xml::__toString()
+	 * @return string
+	 */
+	final protected function tag() {return 'result';}
 
 	/**
 	 * 2017-09-13
-	 * @used-by __toString()
+	 * @used-by contents()
 	 * @used-by i()
 	 * @var int
 	 */
